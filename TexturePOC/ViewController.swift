3f94039fd185c40cb33ccdd9cd132b60388ccacb
Trevor Jones <trevor.jones@dialpad.com>
2022-08-26 14:32:08 -0400

    Initial Commit

---
+++ TexturePOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TexturePOC
+//
+//  Created by Trevor Jones on 2022-08-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
